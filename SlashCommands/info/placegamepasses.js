545597784779c24df52994a8ee2f87a4672fd091
max <93793772+maxhithere@users.noreply.github.com>
2022-09-10 20:30:53 -0400

    Update placegamepasses.js

--- SlashCommands/info/placegamepasses.js
+++ SlashCommands/info/placegamepasses.js
@@ -23,7 +23,6 @@ module.exports = {
      */
     run: async (client, interaction, args) => {
       const game = interaction.options.getString("game");
-        //2888350255
       try {
           
           async function GetRobloxGame(game) {
@@ -32,7 +31,7 @@ module.exports = {
                  console.log('nope')
               }
               let _data = await response.json();
-            //  console.log(_data) //.Id
+           
               return _data.universeId;
               
           }
@@ -44,12 +43,12 @@ module.exports = {
           const gameSocialLinks = await noblox.getGamePasses(pre)
       
           
-         // console.log(gameSocialLinks)
+       
       
          let assetNamesa = gameSocialLinks.map(asset => asset.name + ': ' + asset.price).join("\n")
          let assetNamesb = gameSocialLinks.map(asset => asset.productId).join("\n")
       
-         //console.log(assetNamesa)
+       
           if(!assetNamesa) assetNamesa = 'none'
           if(!assetNamesb) assetNamesb = 'none'
           const embed = new MessageEmbed()
