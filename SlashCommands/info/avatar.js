00e7394c6bf943673eceed2c842c582b49ac6c38
max <93793772+maxhithere@users.noreply.github.com>
2022-09-10 20:22:35 -0400

    Update avatar.js

--- SlashCommands/info/avatar.js
+++ SlashCommands/info/avatar.js
@@ -26,26 +26,23 @@ module.exports = {
         
           let user = interaction.options.getString("user")
          let information2 = await noblox.getIdFromUsername(user) 
-   // if(user == NaN) {
-     //   message.channel.send('Please provide a valid user **ID**')
-   // } 
+   
 try {
     const wearingAssets = await noblox.getAvatar(information2)
     let desc = " ";
  const assetNamesa = wearingAssets.assets.map(asset => asset.name)
  for(let i = 0; i < assetNamesa.length; i++) {
- let number = assetNamesa[i];
+ let number = assetNamesa[i];                 //idk why i was doing this method 
  desc += (`${number}\n`)
- }
+ }                                  
     const embed = new MessageEmbed()
     .setTitle(`Information on avatar **${information2}**`)
    
     .addField('Avatar Type', `${wearingAssets.playerAvatarType}`)
-    //.addField('Assets', `${assetNames}`)
     .addField('Assets', `${desc}`)
     .addField('Default Shirt', `${wearingAssets.defaultShirtApplied}`)
     .addField('Default Pants', `${wearingAssets.defaultPantsApplied}`)
-   // .addField('Emotes', `${wearingAssets.emotes}`)
+   
     .setTimestamp()
         interaction.reply({embeds: [embed]})
 } catch(err) {
